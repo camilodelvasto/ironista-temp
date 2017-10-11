cb2e9ea39c6b98886cbbf9f36ad0f472f1996b27
Camilo Delvasto <camilodelvasto@gmail.com>
2017-10-11 12:53:10 -0500

    Add response page

---
+++ pages/success.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="index">
+    <section id="contacto">
+      <div class="container">
+        <div class="form-intro">
+          <h1>Hemos recibido su mensaje</h1>
+          <p>Gracias, {{ senderName }}, por su mensaje. En breve nos pondremos en contacto.</p>
+        </div>
+        <div>
+        </div>
+      </div>
+    </section>
+
+    <BlackFooter />
+  </div><!--container-->
+</template>
+
+<script>
+import BlackFooter from '~/components/BlackFooter.vue'
+
+export default {
+  transition: 'page',
+  components: {
+    BlackFooter
+  },
+  computed: {
+    senderName () {
+      return this.$route.query.name
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.form-intro {
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+
+form {
+  @include breakpoint($sm) {
+    width: 60%;
+    margin: 0 auto;
+  }
+}
+
+</style>
